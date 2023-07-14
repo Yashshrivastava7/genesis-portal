e82b4126d2a696b8b2db78d9eefc68ded4091c2c
Yash Shrivastava <yash.shrivastava77@gmail.com>
2023-07-14 18:25:06 +0530

    Check for session on events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,7 +1,19 @@
-export default function Page() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function Events() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("./api/auth/signin");
+  }
+
+  console.log(session);
   return (
     <>
-      <h1>Hello from Events page</h1>
+      <h1>Hello from Events Page!</h1>
+      <pre>{JSON.stringify(session)}</pre>
     </>
   );
 }
