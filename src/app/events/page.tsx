ab065e438c7c2eee28da79fee669023178d64f68
Yash Shrivastava <yash.shrivastava77@gmail.com>
2023-07-16 15:54:52 +0530

    Add key prop to h1 tags in events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -26,10 +26,10 @@ export default async function Events() {
       <h1 className="text-center m-3">Hello from Events Page!</h1>
       <pre className="text-center m-3">{JSON.stringify(session)}</pre>
       <div className="flex flex-col justify-center items-center text-center">
-        {events.map((event) => (
+        {events.map((event: eventType) => (
           <div className="border-solid border-2 m-1 w-40">
-            <h1>{event.author}</h1>
-            <h1>{event.title}</h1>
+            <h1 key={event.id}>{event.author}</h1>
+            <h1 key={event.id}>{event.title}</h1>
           </div>
         ))}
       </div>
