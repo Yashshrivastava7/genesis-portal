3bf087e847b5268ff900aa0cfaaf458899d26de5
Yash Shrivastava <yash.shrivastava77@gmail.com>
2023-07-17 18:39:21 +0530

    Fix indent on admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,15 +1,16 @@
-import { getServerSession } from "next-auth"
-import { authOptions } from "../api/auth/[...nextauth]/route"
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function AdminDashBoard() {
-    const session = await getServerSession(authOptions)
-    if (session.user.role !== "ADMIN") {
-        return <h1>Unauthorized</h1>
-    }
-    return (
-        <>
-            <h1>Hello from Admin DashBoard</h1>
-            <pre className="text-center m-3">{JSON.stringify(session)}</pre>
-        </>
-    );
+  const session = await getServerSession(authOptions);
+  console.log(session);
+  if (session.user.role !== "ADMIN") {
+    return <h1>Unauthorized</h1>;
+  }
+  return (
+    <>
+      <h1 className="text-center m-3">Hello from Admin DashBoard</h1>
+      <pre className="text-center m-3">{JSON.stringify(session)}</pre>
+    </>
+  );
 }
