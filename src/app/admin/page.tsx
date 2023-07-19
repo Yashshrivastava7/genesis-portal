18ce9a4f65efc56a14241f2a7d7720c2bc69b477
aaryansr26 <aaryansr26@gmail.com>
2023-07-19 17:26:59 +0530

    Create Admin Page with Auth

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,6 @@
 import { getServerSession } from "next-auth"
 import { authOptions } from "../api/auth/[...nextauth]/route"
+import Link from "next/link";
 
 export default async function AdminDashBoard() {
     const session = await getServerSession(authOptions)
@@ -10,6 +11,9 @@ export default async function AdminDashBoard() {
         <>
             <h1>Hello from Admin DashBoard</h1>
             <pre className="text-center m-3">{JSON.stringify(session)}</pre>
+
+            <Link href={"/admin/createEvent"} className="bg-cyan-300 rounded-md px-2 py-3">Create a new event</Link>
+
         </>
     );
 }
