30ba2ccb93961643e744cbcb0bc00dd2d4f48a9f
Yash Shrivastava <yash.shrivastava77@gmail.com>
2023-07-21 22:27:36 +0530

    Add some css to events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -42,7 +42,7 @@ export default async function Events() {
       <pre className="text-center m-3">{JSON.stringify(session)}</pre>
       <div className="flex flex-col justify-center items-center text-center">
         {events.map((event: eventType) => (
-          <div className="border-solid border-2 m-1 w-40">
+          <div className="border-solid border-2 m-1 w-52 transition-shadow hover:shadow-lg">
             <h1>{event.author}</h1>
             <h1>{event.title}</h1>
             <h1>{event.content}</h1>
