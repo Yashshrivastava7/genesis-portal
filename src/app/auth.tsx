e80e0e08aeb099123ef034a700762ef33f968e04
Yash Shrivastava <yash.shrivastava77@gmail.com>
2023-07-23 22:31:31 +0530

    Further change type names

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -4,15 +4,13 @@ import { signIn, signOut } from "next-auth/react";
 import { registerEvent } from "./events/page";
 import { useTransition } from "react";
 
-
-
-type newEventType = {
+type eventType = {
   title: string;
   location: string;
   date: string;
   capacity: number;
   content: string;
-}
+};
 
 export function LoginButton() {
   return <Button onClick={() => signIn()}>Login</Button>;
@@ -22,10 +20,9 @@ export function LogoutButton() {
   return <Button onClick={() => signOut()}>Logout</Button>;
 }
 
-
 // New Event Register Button - Aaryan
-export function RegisterEvent(props: newEventType) {
-  let [isPending, startTransition] = useTransition();
+export function RegisterEvent(props: eventType) {
+  let [, startTransition] = useTransition();
   return (
     <Button
       className="m-2"
