2003c40a455292d900e89110a4c04c3ad75eedf2
aaryansr26 <aaryansr26@gmail.com>
2023-07-23 17:46:39 +0530

    Add backend api route to find registrations

---
+++ src/app/api/fetchevent/route.tsx
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+export async function POST(req: Request) {
+    const body = await req.json();
+    console.log(body);
+
+    const prisma = new PrismaClient();
+
+    const users = await prisma.registration.findMany({
+        where: {
+            event: body.title
+        } as any
+    })
+
+    console.log("Server Side => ",  users) 
+    return new Response(JSON.stringify({ message: "successful", users: users }))
+}
